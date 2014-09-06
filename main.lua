c34f3eb6e707e28d5c6ac8362f9862d029a5d600
frtzarn <frtzarn@umich.edu>
2014-09-06 01:26:27 -0400

    added score and height, needs to be changed when controls change

--- main.lua
+++ main.lua
@@ -7,12 +7,20 @@ function love.load()
    spaceship = love.graphics.newImage("assets/spaceship96x96.png")
    rocketTexture = love.graphics.newImage("assets/yellowtexture.png")
    ground = love.graphics.newImage("assets/ground_0.png")
+   groundY = love.graphics:getHeight() - ground:getHeight()
+   
    x = 350
    y = 473
    angle = 0
    speed = 300
    angular_speed = 1
    love.graphics.setBackgroundColor(126, 192, 238)
+   scoreDisplayRoot = "Score: "
+   scoreDisplay = "Score: 0"
+   maxHeight = 0
+   currentHeight = 0
+   scoreX = 50
+   scoreY = 50
 end
 
 function love.update(dt)
@@ -27,14 +35,27 @@ function love.update(dt)
 
    if love.keyboard.isDown("down") then
       y = y + (speed * dt)
+      --this needs to be revised for gravity
+      currentHeight = currentHeight - (speed * dt)
    end
    if love.keyboard.isDown("up") then
       y = y - (speed * dt)
+      currentHeight = currentHeight + (speed * dt)
    end
+
+   if currentHeight > maxHeight then
+		maxHeight = currentHeight
+		scoreDisplay = scoreDisplayRoot .. math.floor(maxHeight)
+	end
 end
 
 function love.draw()
    love.graphics.draw(spaceship, x, y, angle)
+   displayScore()
    -- if current height below the first camera change
-   love.graphics.draw(ground, 0, 570)
+   love.graphics.draw(ground, 0, groundY)
+end
+
+function displayScore()
+	love.graphics.print(scoreDisplay, scoreX, scoreY)
 end
