f2b9923c0db0a0b392d1568bd5fdea98885dc628
frtzarn <frtzarn@umich.edu>
2014-09-06 03:20:58 -0400

    made idiomatic lua changes to local

--- main.lua
+++ main.lua
@@ -5,6 +5,23 @@
 local Vec2 = require "Vec2_ffi"
 require "camera"
 
+local moveCamera = false
+local moveCameraAmount = 0
+local cameraMiddle = love.graphics:getHeight() / 2
+
+local scoreDisplayRoot = "Score: "
+local scoreDisplay = "Score: 0"
+
+local startY
+local maxHeight = 0
+local currentHeight = 0
+
+local scoreX = 50
+local scoreY = 50
+
+local rocketforce = -5000
+
+local ground = {}
 function love.load()
 
 	spaceship = love.graphics.newImage("assets/spaceship96x96.png")
@@ -12,33 +29,17 @@ function love.load()
 	groundImage = love.graphics.newImage("assets/ground_0.png")
 	groundY = love.graphics:getHeight() - groundImage:getHeight()
 
-	cameraMiddle = love.graphics:getHeight() / 2
-	moveCamera = false
-	moveCameraAmount = 0
-
-	x = 350
-	y = 473
-	angle = 0
-	speed = 300
-	angular_speed = 1
 	love.graphics.setBackgroundColor(126, 192, 238)
-	scoreDisplayRoot = "Score: "
-	scoreDisplay = "Score: 0"
 
-	scoreX = 50
-	scoreY = 50
 
 	love.physics.setMeter(64)
 	world = love.physics.newWorld(0, 9.81*64, true)
-	rocketforce = -5000
+	
 	body = love.physics.newBody(world, 650/2, 650/2, "dynamic") --place the body in the center of the world and make it dynamic, so it can move around
 	startY = body:getY()
-	maxHeight = 0
-   	currentHeight = 0
 	shape = love.physics.newRectangleShape(spaceship:getDimensions())
 	fixture = love.physics.newFixture(body, shape, 1) -- Attach fixture to body and give it a density of 1.
 
-	ground = {}
 	ground.body = love.physics.newBody(world, 0, groundY) --remember, the shape (the rectangle we create next) anchors to the body from its center, so we have to move it to (650/2, 650-50/2)
 	ground.shape = love.physics.newRectangleShape(groundImage:getDimensions()) --make a rectangle with a width of 650 and a height of 50
 	ground.fixture = love.physics.newFixture(ground.body, ground.shape) --attach shape to body
@@ -57,7 +58,7 @@ function love.update(dt)
 	currentHeight = startY - body:getY()
 	if currentHeight > maxHeight then
 		maxHeight = currentHeight
-		scoreDisplay = scoreDisplayRoot .. math.floor(maxHeight)
+		scoreDisplay = scoreDisplayRoot .. math.floor(maxHeight / 10)
 	end
 	if body:getY() < cameraMiddle then
 		moveCamera = true
@@ -74,12 +75,12 @@ local multiplied_v = v * 3
 function love.draw()
 	camera:set()
 	displayScore()
-	if (moveCamera) then
-		camera:move(0, moveCameraAmount)
-	end
 	-- if current height below the first camera change
 	love.graphics.draw(groundImage, 0, groundY)
 	love.graphics.draw(spaceship,body:getX(),body:getY(),body:getAngle(),1,1,spaceship:getDimensions() / 2)
+	if (moveCamera) then
+		camera:move(0, moveCameraAmount)
+	end
 	camera:unset()
 end
 
