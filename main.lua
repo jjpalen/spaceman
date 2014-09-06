a232b867fc427540de4807727dace8a6ef55eb09
frtzarn <frtzarn@umich.edu>
2014-09-06 01:01:13 -0400

    drew the sky, ground and placed spaceship correctly

--- main.lua
+++ main.lua
@@ -4,12 +4,15 @@
 local Vec2 = require "Vec2_ffi"
 
 function love.load()
-   spaceship = love.graphics.newImage("spaceship96x96.png")
-   x = 400
-   y = 300
+   spaceship = love.graphics.newImage("assets/spaceship96x96.png")
+   rocketTexture = love.graphics.newImage("assets/yellowtexture.png")
+   ground = love.graphics.newImage("assets/ground_0.png")
+   x = 350
+   y = 473
    angle = 0
    speed = 300
    angular_speed = 1
+   love.graphics.setBackgroundColor(126, 192, 238)
 end
 
 function love.update(dt)
@@ -32,4 +35,6 @@ end
 
 function love.draw()
    love.graphics.draw(spaceship, x, y, angle)
+   -- if current height below the first camera change
+   love.graphics.draw(ground, 0, 570)
 end
