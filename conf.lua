af073de3bf2caff6f5e442a69b9df712f8b7779d
frtzarn <frtzarn@umich.edu>
2014-09-10 12:59:55 -0400

    added config

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+	t.version = "0.9.1"                -- The LÖVE version this game was made for (string)
+    t.console = false                  -- Attach a console (boolean, Windows only)
+
+    t.window.title = "Spaceman"
+    t.window.height = 600
+    t.window.width = 800
+    t.window.icon = "/assets/spaceship96x96.png"
+end
